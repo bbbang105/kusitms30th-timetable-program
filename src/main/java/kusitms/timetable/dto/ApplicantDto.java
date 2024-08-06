6ccc1a244f850e19801f2c90e946eabb7761cf38
bbbang105 <2018111366@dgu.ac.kr>
2024-08-06 18:42:44 +0900

    [Feat] : 지원자 DTO 추가

---
+++ src/main/java/kusitms/timetable/dto/ApplicantDto.java
@@ -0,0 +1,22 @@
+package kusitms.timetable.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+public class ApplicantDto {
+    @Builder
+    @Getter
+    @AllArgsConstructor
+    @JsonNaming(value = PropertyNamingStrategies.SnakeCaseStrategy.class)
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    public static class addApplicantRequest {
+        private String name;
+        private String part;
+        private String startTime;
+        private String endTime;
+    }
+}
