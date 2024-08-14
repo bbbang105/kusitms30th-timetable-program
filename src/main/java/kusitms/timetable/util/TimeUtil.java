b76d63aaedbc3f81969a06c0c7d23c5b10d55499
bbbang105 <2018111366@dgu.ac.kr>
2024-08-14 16:05:24 +0900

    [Feat] : 타임 유틸 기능 추가

--- src/main/java/kusitms/timetable/util/TimeUtil.java
+++ src/main/java/kusitms/timetable/util/TimeUtil.java
@@ -13,18 +13,30 @@ import java.util.List;
 @Component
 @RequiredArgsConstructor
 public class TimeUtil {
+
+    private static final DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("HH:mm");
+
     public List<String> splitTimeIntervals(String startTime, String endTime, String dayCode) {
         List<String> timeIntervals = new ArrayList<>();
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm");
 
-        LocalTime start = LocalTime.parse(startTime, formatter);
-        LocalTime end = LocalTime.parse(endTime, formatter);
+        LocalTime start = LocalTime.parse(startTime, FORMATTER);
+        LocalTime end = LocalTime.parse(endTime, FORMATTER);
 
         while (start.isBefore(end)) {
-            timeIntervals.add(dayCode + start.format(formatter));
+            timeIntervals.add(dayCode + start.format(FORMATTER));
             start = start.plusMinutes(30);
         }
 
         return timeIntervals;
+    }
+
+    public String minusThirtyMinutes(String code) {
+        String dayCode = code.substring(0, 1);
+        String timePart = code.substring(1);
+
+        LocalTime time = LocalTime.parse(timePart, FORMATTER);
+        LocalTime previousTime = time.minusMinutes(30);
+
+        return dayCode + previousTime.format(FORMATTER);
     }
 }
